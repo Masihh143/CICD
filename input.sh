7d9241114b9ca8abf6b3941c2b4f4e2c6ab3ea59
masihh143 <masihuddin4545@gmail.com>
2026-09-24 15:58:49 +0530

    added yaml file

---
+++ input.sh
@@ -0,0 +1,2 @@
+read -p "write your name:" name
+echo "Hello, $name!"
---
+++ variable.sh
@@ -0,0 +1,6 @@
+!#/bin/bash
+
+name="Masih"
+rollno="10570"
+comment="class is really interactive and fun"
+echo "My name is $name and my roll number is $rollno. I think the $comment"
